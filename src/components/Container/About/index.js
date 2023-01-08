ce35d2cbd622da29e3eb2f2be40dc79c6e50f73d
Chaimae-El-beqqal <102841184+Chaimae-El-beqqal@users.noreply.github.com>
2023-01-08 23:21:02 +0100

    portfolio

---
+++ src/components/Container/About/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const About=()=>{
+    return(
+        <div className="about" data-aos="fade-up" id="About">
+
+            <p className="title">00.✨ About Me</p>
+            <p className="text-about">I am Chaimae El beqqal, a computer science engineering student.I have a strong 
+            <br></br>enthusiasm for this area. For me, making web and mobile applications that bring ideas to life
+            <br></br>and in some way benefit people is the best thing ever. </p>
+        </div>
+    )
+}
+export default About
